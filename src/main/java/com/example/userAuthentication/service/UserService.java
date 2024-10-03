1867c0ebd159c45cd135b6ff8d942b5fd309088c
Siddhesh Jondhale <84581657+Siddheshjondhale@users.noreply.github.com>
2024-10-04 04:06:59 +0530

    Hashing of User Info

--- src/main/java/com/example/userAuthentication/service/UserService.java
+++ src/main/java/com/example/userAuthentication/service/UserService.java
@@ -1,6 +1,7 @@
 package com.example.userAuthentication.service;
 
 import com.example.userAuthentication.entity.User;
+import com.example.userAuthentication.hashing.HashUtil;
 import com.example.userAuthentication.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,11 +13,13 @@ public class UserService {
     private UserRepository userRepository;
 
     public User registerOrUpdateUser(String email, String googleId, String name) {
+        String hashUserId= HashUtil.hash(googleId,16);
         User user = userRepository.findByGoogleId(googleId);
+
         if (user == null) { // If user doesn't exist, create a new one
             user = new User();
             user.setEmail(email);
-            user.setGoogleId(googleId);
+            user.setGoogleId(hashUserId);
             user.setName(name);
             return userRepository.save(user);
         }
